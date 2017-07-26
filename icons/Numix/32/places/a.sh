8ea37cc177a8698eeda5ec11456edc2806617a19
Madh93 <michelangelo93@gmail.com>
2017-07-26 15:15:50 +0100

    Default themes

---
+++ icons/Numix/32/places/a.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#folder_color_vaia_desktop.svg -> vaia-user-desktop.svg
+#folder_color_vaia_documents.svg -> vaia-folder-documents.svg
+#folder_color_vaia_downloads.svg -> vaia-folder-download.svg
+#folder_color_vaia_music.svg -> vaia-folder-music.svg
+#folder_color_vaia_pictures.svg -> vaia-folder-pictures.svg
+#folder_color_vaia_public.svg -> vaia-folder-publicshare.svg
+#folder_color_vaia.svg -> vaia-folder.svg
+#folder_color_vaia_templates.svg -> vaia-folder-templates.svg
+#folder_color_vaia_videos.svg -> vaia-folder-video.svg
+
+
+ln -s vaia-user-desktop.svg folder_color_vaia_desktop.svg
+ln -s vaia-folder-documents.svg folder_color_vaia_documents.svg
+ln -s vaia-folder-download.svg folder_color_vaia_downloads.svg
+ln -s vaia-folder-music.svg folder_color_vaia_music.svg
+ln -s vaia-folder-pictures.svg folder_color_vaia_pictures.svg
+ln -s vaia-folder-publicshare.svg folder_color_vaia_public.svg
+ln -s vaia-folder.svg folder_color_vaia.svg
+ln -s vaia-folder-templates.svg folder_color_vaia_templates.svg
+ln -s vaia-folder-video.svg folder_color_vaia_videos.svg
